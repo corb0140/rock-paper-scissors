4b5c845775e745fdaeb5da871b0556bbb6eaedf5
Mark Corbin <markpc1608@gmail.com>
2024-08-15 17:13:21 -0400

    responsive home page

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,25 +6,29 @@ export default function Header() {
   const score = useSelector((state) => state.game.score);
 
   return (
-    <header className="h-auto p-4">
-      <div className="h-auto border-4 border-headerOutline rounded-lg p-5 flex justify-between">
+    <header className="h-auto p-4 lg:px-[25rem]">
+      <div className="h-auto border-4 border-headerOutline rounded-lg p-5 lg:p-10 flex justify-between">
         {/* ROCK, PAPER, SCISSORS */}
-        <div className="flex flex-col h-[4.5rem]">
-          <h1 className="uppercase font-bold text-2xl leading-5 text-white">
+        <div className="flex flex-col h-[4.5rem] lg:h-[6.5rem]">
+          <h1 className="uppercase font-bold text-2xl lg:text-4xl leading-5 text-white">
             Rock
           </h1>
-          <h1 className="uppercase font-bold text-2xl leading-5 text-white">
+          <h1 className="uppercase font-bold text-2xl lg:text-4xl leading-5 text-white">
             Paper
           </h1>
-          <h1 className="uppercase font-bold text-2xl leading-5 text-white">
+          <h1 className="uppercase font-bold text-2xl lg:text-4xl leading-5 text-white">
             Scissors
           </h1>
         </div>
 
         {/* SCORE */}
-        <div className="bg-white flex flex-col leading-5 items-center justify-center w-[4.5rem] h-[4.5rem] rounded-lg">
-          <p className="text-scoreText font-sm tracking-wider">score</p>
-          <p className="text-darkText text-4xl font-bold">{score}</p>
+        <div className="bg-white flex flex-col leading-5 items-center justify-center w-[4.5rem] lg:w-[6.5rem] h-[4.5rem] lg:h-[6.5rem] rounded-lg">
+          <p className="text-scoreText text-sm lg:text-lg tracking-wider">
+            score
+          </p>
+          <p className="text-darkText text-4xl lg:text-6xl font-bold">
+            {score}
+          </p>
         </div>
       </div>
     </header>
--- app/page.js
+++ app/page.js
@@ -31,13 +31,13 @@ export default function Home() {
   };
 
   return (
-    <main className="h-[600px] w-full flex flex-col items-center justify-around px-10">
-      <div className="h-[250px] w-full flex items-center justify-center relative mt-10">
+    <main className="relative h-[600px] lg:h-[500px] w-full flex flex-col items-center justify-around px-10 lg:px-[30rem]">
+      <div className="h-[250px] lg:h-[350px] w-full flex items-center justify-center relative mt-10">
         {/* PAPER SCISSORS */}
         <div className="flex justify-between w-full absolute top-0 left-0 z-10">
           <div
-            className="bg-white border-[12px] border-paperColor rounded-full h-[120px] w-[120px] flex items-center justify-center
-          shadow-[inset_0px_5px_1px_rgba(0,0,0,.1),0px_6px_0px_hsl(230,89%,62%)]"
+            className="lg:absolute lg:-right-5 lg:-top-10 bg-white border-[12px] lg:border-[15px] border-paperColor rounded-full h-[180px] w-[180px] 
+            flex items-center justify-center shadow-[inset_0px_5px_1px_rgba(0,0,0,.1),0px_6px_0px_hsl(230,89%,62%)]"
             onClick={() => goToGame("paper")}
           >
             <Image
@@ -50,8 +50,8 @@ export default function Home() {
           </div>
 
           <div
-            className="bg-white border-[12px] border-scissorsColor rounded-full h-[120px] w-[120px] flex items-center justify-center
-          shadow-[inset_0px_5px_1px_rgba(0,0,0,.1),0px_6px_0px_hsl(39,89%,49%)]"
+            className="lg:absolute lg:-left-5 lg:-top-10 bg-white border-[12px] lg:border-[15px] border-scissorsColor rounded-full h-[180px] w-[180px] 
+            flex items-center justify-center shadow-[inset_0px_5px_1px_rgba(0,0,0,.1),0px_6px_0px_hsl(39,89%,49%)]"
             onClick={() => goToGame("scissors")}
           >
             <Image
@@ -70,13 +70,13 @@ export default function Home() {
           alt="image of triangle"
           width={0}
           height={0}
-          className="h-[65%] w-[65%]"
+          className="h-[65%] w-[65%] lg:h-[100%] lg:w-[100%]"
         />
 
         {/* ROCK */}
         <div className="absolute bottom-0 w-full flex items-center justify-center">
           <div
-            className="bg-white border-[12px] border-rockColor rounded-full h-[120px] w-[120px] flex items-center justify-center
+            className="bg-white border-[12px] lg:border-[15px] border-rockColor rounded-full h-[180px] w-[180px] flex items-center justify-center
           shadow-[inset_0px_5px_1px_rgba(0,0,0,.1),0px_6px_0px_hsl(349,71%,52%)]"
             onClick={() => goToGame("rock")}
           >
@@ -91,7 +91,8 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col gap-y-2">
+      {/* BUTTONS */}
+      <div className="flex flex-col gap-y-2 lg:absolute lg:bottom-10 lg:right-10">
         <button
           className="border-2 border-darkText rounded-lg py-2 px-10 text-white uppercase tracking-wider font-semibold"
           onClick={toggleModal}
